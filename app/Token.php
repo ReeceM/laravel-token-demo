7f02803f67d444a325f4702457cf5f7737140eaa
ReeceM <reecestevenmay@gmail.com>
2019-12-20 00:53:41 +0200

    wip: replace the model class information

--- app/Token.php
+++ app/Token.php
@@ -2,9 +2,52 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Token extends Model
 {
-    //
+    use SoftDeletes;
+    /**
+     * Fillable attributes
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'api_token',
+        'limit'
+    ];
+
+    /**
+     * The things that are hidden
+     *
+     * @var array
+     */
+    protected $hidden = ['api_token'];
+
+    /**
+     * The user relation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * THe stats of the token
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tokenStatistic(): HasMany
+    {
+        return $this->hasMany(TokenStatistic::class);
+    }
 }
--- app/TokenStatistic.php
+++ app/TokenStatistic.php
@@ -6,5 +6,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class TokenStatistic extends Model
 {
-    //
+    /**
+     * The fillable values
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'date',
+        'success',
+        'ip_address',
+        'token_id',
+        'request',
+    ];
+
+    /**
+     * Casting array
+     *
+     * @var array
+     */
+    protected $casts = [
+        'success' => 'bool',
+        'request' => 'collection',
+    ];
+
+    /**
+     * Doesn't have timestamps
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
